b3dc116f5e78f298b4d7e068e430f737ae2947bf
Eslam Medhat <28203059+madahetooo@users.noreply.github.com>
2023-07-05 20:55:42 +0300

    Initial commit

---
+++ app/src/main/java/com/bamboogeeks/julyacademy2/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.bamboogeeks.julyacademy2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            // Navigate from this class to Main Activity Class
+           val intent =  Intent(this,LoginScreen::class.java)
+            startActivity(intent)// launcher
+            finish()     // Kill Splash Screen
+        },4000)
+
+    }
+}
