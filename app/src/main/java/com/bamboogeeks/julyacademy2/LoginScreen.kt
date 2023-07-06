b51ba5bf2bdcabc6eab945ad78e514db83fb27af
Eslam Medhat <28203059+madahetooo@users.noreply.github.com>
2023-07-06 19:36:26 +0300

    Add Registration Screen

--- app/src/main/java/com/bamboogeeks/julyacademy2/LoginScreen.kt
+++ app/src/main/java/com/bamboogeeks/julyacademy2/LoginScreen.kt
@@ -1,11 +1,25 @@
 package com.bamboogeeks.julyacademy2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bamboogeeks.julyacademy2.databinding.ActivityLoginScreenBinding
 
 class LoginScreen : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_screen)
+        binding = ActivityLoginScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        binding.tvRegister.setOnClickListener {
+            val intent = Intent(this,RegistrationScreen::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/bamboogeeks/julyacademy2/RegistrationScreen.kt
@@ -0,0 +1,26 @@
+package com.bamboogeeks.julyacademy2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bamboogeeks.julyacademy2.databinding.ActivityRegistrationScreenBinding
+
+class RegistrationScreen : AppCompatActivity() {
+    private lateinit var binding: ActivityRegistrationScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegistrationScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnRegister.setOnClickListener {
+         val intent =   Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        binding.tvLogin.setOnClickListener {
+            val intent =   Intent(this,LoginScreen::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
