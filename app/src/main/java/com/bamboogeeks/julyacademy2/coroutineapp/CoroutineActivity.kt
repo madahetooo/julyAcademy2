11b1049dae48f9b70ab734e03cf6254198a54a4c
Eslam Medhat <28203059+madahetooo@users.noreply.github.com>
2023-07-13 19:22:50 +0300

    Added CoroutineActivity

--- app/src/main/java/com/bamboogeeks/julyacademy2/coroutineapp/CoroutineActivity.kt
+++ app/src/main/java/com/bamboogeeks/julyacademy2/coroutineapp/CoroutineActivity.kt
@@ -8,6 +8,8 @@ import com.bamboogeeks.julyacademy2.R
 import com.bamboogeeks.julyacademy2.databinding.ActivityCoroutineBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -19,17 +21,38 @@ class CoroutineActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCoroutineBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Log.d(TAG,"This is Thread ${Thread.currentThread().name}")
 
-        CoroutineScope(Dispatchers.IO).launch {
-            delay(5000L)
-            Log.d(TAG,"This is  Thread ${Thread.currentThread().name}")
+        GlobalScope.launch(Dispatchers.IO) {
 
-            withContext(Dispatchers.Main){
-                binding.txDummyText.text = "This is New Dummy Text"
-            }
+            val result1 = async { doNetworkCall1() }
+            val result2 = async { doNetworkCall2() }
+            Log.d(TAG,"Result 1 is : ${result1.await()}")
+            Log.d(TAG,"Result 1 is : ${result2.await()}")
+
+
+//            var result1:String? =null
+//            var result2:String? =null
+//
+//            launch(Dispatchers.IO) {
+//                result1 =  doNetworkCall1()
+//            }
+//
+//            launch(Dispatchers.IO) {
+//                result2 =  doNetworkCall2()
+//            }
+//
+//            Log.d(TAG,"Result 1 is equal to : ${result1}")
+//            Log.d(TAG,"Result 2 is equal to : ${result2}")
 
         }
 
+    }
+    suspend fun doNetworkCall1():String{
+        delay(4000L)
+        return "Network Call Result 1"
+    }
+    suspend fun doNetworkCall2():String{
+        delay(4000L)
+        return "Network Call Result 2"
     }
 }
